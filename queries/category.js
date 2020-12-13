cf733f351a8138e3076724d1ea0d63c32d9137dd
rongvangc <rongvangc@users.noreply.github.com>
2020-12-14 00:06:17 +0700

    Update Query Cat

--- queries/category.js
+++ queries/category.js
@@ -2,48 +2,46 @@ import { gql } from '@apollo/client';
 
 export const MENU = gql`
   query Menu($items: Int, $catSlug: String) {
-    products(first: $items, where: { category: $catSlug }) {
-      edges {
-        node {
+    products(first: $items, where: {category: $catSlug}) {
+    edges {
+      node {
+        id
+        name
+        sku
+        slug
+        image {
+          altText
+          uri
+          sourceUrl(size: SHOP_THUMBNAIL)
+        }
+        type
+        productCategories {
+          nodes {
+            databaseId
+            id
+            name
+            slug
+          }
+        }
+        ... on VariableProduct {
           id
           name
-          sku
-          slug
-          featured
-          date
-          image {
-            sourceUrl(size: WOOCOMMERCE_THUMBNAIL)
-            altText
-          }
-          productCategories {
-            edges {
-              node {
-                id
-                name
-                slug
-              }
-            }
-          }
-          ... on VariableProduct {
-            id
-            name
-            price(format: FORMATTED)
-          }
-          ... on ExternalProduct {
-            id
-            name
-            price(format: FORMATTED)
-          }
-          ... on GroupProduct {
-            id
-            name
-            price
-          }
-          ... on SimpleProduct {
-            id
-            name
-            price(format: FORMATTED)
-          }
+          price(format: FORMATTED)
+        }
+        ... on ExternalProduct {
+          id
+          name
+          price(format: FORMATTED)
+        }
+        ... on SimpleProduct {
+          id
+          name
+          price(format: FORMATTED)
+        }
+        ... on GroupProduct {
+          id
+          name
+          price
         }
       }
     }
